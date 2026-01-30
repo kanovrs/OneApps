3f619026441a594c5d3636e24e743938e02ee2f9
Ilyas Bezhanidze <spensdee@gmail.com>
2026-01-30 17:07:56 +0200

    static mode

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [360, 640, 768, 1024, 1280, 1536, 1920],
     imageSizes: [16, 32, 64, 128, 256, 384],
